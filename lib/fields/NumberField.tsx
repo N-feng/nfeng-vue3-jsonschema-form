54e90240a5ded5b309223aa82208e7d8cbdad9c2
N-feng <308561157@qq.com>
2022-04-28 16:56:59 +0800

    feat✨:  theme done!

--- lib/fields/NumberField.tsx
+++ lib/fields/NumberField.tsx
@@ -1,14 +1,16 @@
-import { FieldPropsDefine } from '../types'
+import { FieldPropsDefine, CommonWidgetNames } from '../types'
 import { defineComponent } from 'vue'
+
+import { getWidget } from '../theme'
 
 export default defineComponent({
   name: 'NumberField',
   props: FieldPropsDefine,
   setup(props) {
-    const handleChange = (e: any) => {
-      const value = e.target.value
+    const handleChange = (v: string) => {
+      // const value = e.target.value
 
-      const num = Number(value)
+      const num = Number(v)
 
       if (Number.isNaN(num)) {
         props.onChange(undefined)
@@ -16,8 +18,14 @@ export default defineComponent({
         props.onChange(num)
       }
     }
-    return () => (
-      <input type="number" value={props.value} onInput={handleChange} />
-    )
+
+    const NumberWidgetRef = getWidget(CommonWidgetNames.NumberWidget)
+
+    return () => {
+      const NumberWidget = NumberWidgetRef.value
+      const { schema, rootSchema, ...rest } = props
+      // <input type="number" value={props.value} onInput={handleChange} />
+      return <NumberWidget {...rest} onChange={handleChange} />
+    }
   },
 })
--- lib/fields/StringField.tsx
+++ lib/fields/StringField.tsx
@@ -1,16 +1,29 @@
-import { FieldPropsDefine } from '../types'
 import { defineComponent } from 'vue'
+
+import { FieldPropsDefine, CommonWidgetNames } from '../types'
+import { getWidget } from '../theme'
 
 export default defineComponent({
   name: 'StringField',
   props: FieldPropsDefine,
   setup(props) {
-    const handleChange = (e: any) => {
-      const value = e.target.value
-      props.onChange(e.target.value)
+    const handleChange = (v: string) => {
+      // console.log(e)
+      // props.onChange(v)
     }
-    return () => (
-      <input type="text" value={props.value} onInput={handleChange} />
-    )
+
+    const TextWidgetRef = getWidget(CommonWidgetNames.TextWidget)
+
+    return () => {
+      const { schema, rootSchema, ...rest } = props
+
+      const TextWidget = TextWidgetRef.value
+
+      return <TextWidget {...rest} onChange={handleChange} />
+
+      // return (
+      //   <input type="text" value={props.value as any} onInput={handleChange} />
+      // )
+    }
   },
 })
